23eb17488a8115d1f5bf8cd92371cc2c148c5feb
kbellouard <killian.bellouard@getnobullshit.com>
2024-09-25 17:24:07 +0200

    fix(condition model)

--- models/Condition.js
+++ models/Condition.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../sequelize/database');
+const sequelize = require('../server/sequelize/database');
 
 const Condition = sequelize.define('Condition', {
     id: {
